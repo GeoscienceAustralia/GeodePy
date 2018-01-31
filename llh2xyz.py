4a74a906230f43d76b9c24aa4b5720351a1671e3
BatchelorJ <josh.batchelor86@gmail.com>
2018-01-31 16:42:51 +1100

    Updated llh2xyz function

--- llh2xyz.py
+++ llh2xyz.py
@@ -1,24 +1,30 @@
 from decimal import *
 from math import sqrt, sin, cos, radians
-def llh2xyz(lat,long,ellht):
+
+
+def llh2xyz(lat, long, ellht):
     """
-    Write Desc"""
+    Input: Latitude and Longitude in Decimal Degrees, Ellipsoidal Height in metres
+    Output: Cartesian X, Y, Z Coordinates in metres
+    """
     # Convert lat & long to radians
     lat = radians(lat)
     long = radians(long)
     # Universal Transverse Mercator Projection Parameters
-    Proj = [6378137,Decimal('298.25722210088'),500000,10000000,Decimal('0.9996'),6,-177]
+    Proj = [6378137, Decimal('298.25722210088'), 500000,
+            10000000, Decimal('0.9996'), 6, -177]
     # Calculate Projection Constants
     f = float(1/Proj[1])
     a = Proj[0]
     b = a*(1-f)
+    e2 = f*(2 - f)
     # Calculate Rn
     if lat == 0:
         Rn = Proj[0]
     else:
-        Rn = a/(sqrt((b**2/a**2)*(sin(lat)**2)))
+        Rn = a/(sqrt(1 - e2 * (sin(lat)**2)))
     # Calculate x, y, z
     x = Decimal(str((Rn + ellht) * cos(lat) * cos(long)))
     y = Decimal(str((Rn + ellht) * cos(lat) * sin(long)))
     z = Decimal(str(((b**2 / a**2) * Rn + ellht) * sin(lat)))
-    return(x, y, z)
+    return x, y, z
