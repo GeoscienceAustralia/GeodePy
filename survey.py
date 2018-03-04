caf762e66b23f477906c9a9a4dbc0852cbbc8852
BatchelorJ <josh.batchelor86@gmail.com>
2018-03-05 09:03:47 +1100

    Initial work on survey module

--- survey.py
+++ survey.py
@@ -45,56 +45,46 @@ def va_conv(verta_hp, slope_dist, height_inst=0, height_tgt=0):
         verta_pt_hp = dd2dms(degrees(verta_pt) + 90)
     return verta_pt_hp, slope_dist_pt, hz_dist, delta_ht
 
-class station(object):
-    def __init__(self, name, hz_datum=GDA2020, vert_datum=AHD, east, north, height, inst_height):
-        self.name = name
-        self.hz_datum = hz_datum
-        self.vert_datum = vert_datum
-        self.east = east
-        self.north = north
-        self.height = height
-        self.inst_height = inst_height
+
+to_stn = ['GA03', 'GA02', 'SYM2', 'ATWR', 'MAHON', 'MAHON', 'ATWR', 'SYM2', 'GA02', 'GA03']
+flfr = [359.59597, 14.48289, 25.35515, 215.57043, 300.59046, 120.59060, 35.57045, 205.35530, 194.48282, 179.59581]
+flfr_vert = [90.30228, 90.09547, 89.50396, 88.03600, 87.58014, 272.01587, 271.55593, 270.09227, 269.50091, 269.29414]
 
 
-# to_stn = ['GA03', 'GA02', 'SYM2', 'ATWR', 'MAHON', 'MAHON', 'ATWR', 'SYM2', 'GA02', 'GA03']
-# flfr = [359.59597, 14.48289, 25.35515, 215.57043, 300.59046, 120.59060, 35.57045, 205.35530, 194.48282, 179.59581]
-# flfr_vert = [90.30228, 90.09547, 89.50396, 88.03600, 87.58014, 272.01587, 271.55593, 270.09227, 269.50091, 269.29414]
-#
-#
-# def hz_round(brg_list):
-#     """
-#     Input: an even palindromic list of horizontal angle observations in two faces
-#     (e.g. [fl1, fl2, fl3, fr3, fr2, fr1])
-#     Output: an averaged face-left sense list of horizontal angles.
-#     """
-#     brg_avg = []
-#     obs = int((len(brg_list))/2)
-#     for i in range(0, obs):
-#         hz_avg = (dms2dd(brg_list[i]) + (dms2dd(brg_list[-(i+1)])-180))/2
-#         brg_avg.append(round(dd2dms(hz_avg), 7))
-#     return brg_avg
-#
-#
-# def va_round(va_list):
-#     """
-#     Input: an even palindromic list of vertical angle observations in two faces
-#     (e.g. [fl1, fl2, fl3, fr3, fr2, fr1])
-#     Output: an averaged face-left sense list of vertical angles.
-#     """
-#     va_avg = []
-#     obs = int((len(va_list))/2)
-#     for i in range(0, obs):
-#         fl_ang = dms2dd(va_list[i]) - 90
-#         fr_ang = 270 - dms2dd(va_list[-(i+1)])
-#         ang_avg = (fl_ang + fr_ang)/2 + 90
-#         va_avg.append(round(dd2dms(ang_avg), 7))
-#     return va_avg
-#
-#
-# # Test for round of obs
-# if to_stn == to_stn[::-1] and len(to_stn) % 2 == 0:
-#     brg_avg = hz_round(flfr)
-#     va_avg = va_round(flfr_vert)
-#     to_stn_avg = to_stn[0:int(len(to_stn)/2)]
-# else:
-#     brg_avg = ['nope']
+def hz_round(brg_list):
+    """
+    Input: an even palindromic list of horizontal angle observations in two faces
+    (e.g. [fl1, fl2, fl3, fr3, fr2, fr1])
+    Output: an averaged face-left sense list of horizontal angles.
+    """
+    brg_avg = []
+    obs = int((len(brg_list))/2)
+    for i in range(0, obs):
+        hz_avg = (dms2dd(brg_list[i]) + (dms2dd(brg_list[-(i+1)])-180))/2
+        brg_avg.append(round(dd2dms(hz_avg), 7))
+    return brg_avg
+
+
+def va_round(va_list):
+    """
+    Input: an even palindromic list of vertical angle observations in two faces
+    (e.g. [fl1, fl2, fl3, fr3, fr2, fr1])
+    Output: an averaged face-left sense list of vertical angles.
+    """
+    va_avg = []
+    obs = int((len(va_list))/2)
+    for i in range(0, obs):
+        fl_ang = dms2dd(va_list[i]) - 90
+        fr_ang = 270 - dms2dd(va_list[-(i+1)])
+        ang_avg = (fl_ang + fr_ang)/2 + 90
+        va_avg.append(round(dd2dms(ang_avg), 7))
+    return va_avg
+
+
+# Test for round of obs
+if to_stn == to_stn[::-1] and len(to_stn) % 2 == 0:
+    brg_avg = hz_round(flfr)
+    va_avg = va_round(flfr_vert)
+    to_stn_avg = to_stn[0:int(len(to_stn)/2)]
+else:
+    brg_avg = ['nope']
