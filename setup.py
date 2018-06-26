acc39305e7d4a71a9286c20d84aa256e0a88d97b
Jonathan Mettes <jonathan@jmettes.com>
2018-06-26 13:51:56 +1000

    Add to PyPI

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='geodepy',
+      version='0.1',
+      description='GA Geodesy Package',
+      url='https://github.com/GeoscienceAustralia/GeodePy',
+      author='Jonathan Mettes',
+      author_email='jonathan.mettes@ga.gov.au',
+      license='CC-BY-4.0',
+      packages=['geodepy'],
+      install_requires=['numpy', 'scipy'])
+
