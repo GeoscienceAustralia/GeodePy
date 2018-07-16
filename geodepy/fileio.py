2d067e94a3dc0aa105c9fca188337e62a22a539b
harry093 <craigdharrison@icloud.com>
2018-07-16 14:43:52 +1000

    Created fileio module

--- geodepy/fileio.py
+++ geodepy/fileio.py
@@ -7,13 +7,15 @@ File input/output module
 In Development
 """
 
+from numpy import zeros
+
 
 def rd_snx_est(file):
-    """This function reads in the SOLUTION/ESTIMATE block of the given SINEX
-    file. It returns solEstimate, a list of tuples:
+    """This function reads in the SOLUTION/ESTIMATE block of a SINEX file. It
+    returns estimate, a list of tuples:
 
-    solEstimate = [(code, soln, refEpoch, staX, staY, staZ, staX_sd, staY_sd,
-                    staZ_sd, velX, velY, velZ, velX_sd, velY_sd, velZ_sd)...]
+    estimate = [(code, soln, refEpoch, staX, staY, staZ, staX_sd, staY_sd,
+                 staZ_sd [, velX, velY, velZ, velX_sd, velY_sd, velZ_sd])...]
 
     where:
         * code is the stations's 4-character ID
@@ -26,26 +28,134 @@ def rd_snx_est(file):
         * vel[XYZ] is the station velocity in the Cartesian reference frame
         * vel[XYZ]_sd
 
+    Velocities are not included in all SINEX files and so are only returned if
+    present.
+
     :param file: the input SINEX file
-    :return: solEstimate
+    :return: estimate
     """
 
-    # Open and read in the given SINEX file
-    with open(file) as f:
-        lines = f.readlines()
-
-    # Read the solution estimates into a list
-    solEstimate = []
+    # Create data structures and set variables
+    lines = []
+    estimate = []
+    check_types = True
+    velocities = False
     go = False
+    code = ''
+    soln = ''
+    epoch = ''
+    stax = ''
+    stay = ''
+    staz = ''
+    stax_sd = ''
+    stay_sd = ''
+    staz_sd = ''
+    velx = ''
+    vely = ''
+    velz = ''
+    velx_sd = ''
+    vely_sd = ''
+    velz_sd = ''
+
+    # Read the SOLUTION/ESTIMATE block into a list and determine if there is
+    # any velocity information
+    with open(file) as f:
+        for line in f:
+            if line[:18] == '-SOLUTION/ESTIMATE':
+                break
+            if go and line[:11] == '*INDEX TYPE':
+                pass
+            elif go:
+                if check_types:
+                    if line[7:10] == 'VEL':
+                        velocities = True
+                        check_types = False
+                lines.append(line)
+            if line[:18] == '+SOLUTION/ESTIMATE':
+                go = True
+
+    if velocities:
+        for line in lines:
+            typ = line[7:11]
+            if typ == 'STAX':
+                code = line[14:18]
+                soln = line[23:26].lstrip()
+                epoch = line[27:39]
+                stax = float(line[47:68])
+                stax_sd = float(line[69:80])
+            elif typ == 'STAY':
+                stay = float(line[47:68])
+                stay_sd = float(line[69:80])
+            elif typ == 'STAZ':
+                staz = float(line[47:68])
+                staz_sd = float(line[69:80])
+            elif typ == 'VELX':
+                velx = float(line[47:68])
+                velx_sd = float(line[69:80])
+            elif typ == 'VELY':
+                vely = float(line[47:68])
+                vely_sd = float(line[69:80])
+            elif typ == 'VELZ':
+                velz = float(line[47:68])
+                velz_sd = float(line[69:80])
+                info = (code, soln, epoch, stax, stay, staz, stax_sd,
+                        stay_sd, staz_sd, velx, vely, velz, velx_sd,
+                        vely_sd, velz_sd)
+                estimate.append(info)
+    else:
+        for line in lines:
+            typ = line[7:11]
+            if typ == 'STAX':
+                code = line[14:18]
+                soln = line[23:26].lstrip()
+                epoch = line[27:39]
+                stax = float(line[47:68])
+                stax_sd = float(line[69:80])
+            elif typ == 'STAY':
+                stay = float(line[47:68])
+                stay_sd = float(line[69:80])
+            elif typ == 'STAZ':
+                staz = float(line[47:68])
+                staz_sd = float(line[69:80])
+                info = (code, soln, epoch, stax, stay, staz, stax_sd,
+                        stay_sd, staz_sd)
+                estimate.append(info)
+
+    return estimate
+
+def rd_snx_mat(file):
+
+    # Read in the codes (station names) and solutions, and check for velocities
+    data = rd_snx_est(file)
+    code_solns = []
+    for stat in data:
+        code_solns.append((stat[0], stat[1]))
+    if len(data[0]) == 15:
+        velocities = True
+
+    # Read the SOLUTION/MATRIX_ESTIMATE block into a list and determine if the
+    # matrix is upper or lower triangular
+    lines = []
+    go = False
+    with open(file) as f:
+        for line in f:
+            if line[:25] == '-SOLUTION/MATRIX_ESTIMATE':
+                break
+            if go and line[:12] == '*PARA1 PARA2':
+                pass
+            elif go:
+                lines.append(line)
+            if line[:25] == '+SOLUTION/MATRIX_ESTIMATE':
+                low_up = line[26]
+                go = True
+
+    # Create the VCV matrix
+    if velocities:
+        n = 6 * int(len(code_solns))
+    else:
+        n = 3 * int(len(code_solns))
+    vcv = zeros((n, n))
     for line in lines:
-        if line[:18] == '-SOLUTION/ESTIMATE':
-            go = False
-        if go:
-            if component == 'BOTH':
-                solEstimate.append(tuple(line.split()))
-            else:
-                if line[7:10] == component:
-                    solEstimate.append(tuple(line.split()))
-        if line[:11] == '*INDEX TYPE':
-            go = True
-    return solEstimate
+        print(line)
+
+    return code_solns
