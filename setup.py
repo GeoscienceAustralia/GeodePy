a13b0c664e4eca0b2df231c7474c735dda1a4989
Jonathan Mettes <jonathan@jmettes.com>
2018-12-12 10:48:33 +1100

    Bump PyPI version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='geodepy',
-      version='0.0.1',
+      version='0.0.2',
       description='GA Geodesy Package',
       url='https://github.com/GeoscienceAustralia/GeodePy',
-      author='Jonathan Mettes',
-      author_email='jonathan.mettes@ga.gov.au',
+      author='Geoscience Australia',
+      author_email='geodesy@ga.gov.au',
       license='Apache License 2.0',
       packages=['geodepy'],
       install_requires=['numpy'])
