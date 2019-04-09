9086b02403a040d1488153fa448449627c2bb391
Jonathan Mettes <jonathan@jmettes.com>
2019-04-09 12:04:37 +1000

    Bump PyPI version to 0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='geodepy',
-      version='0.0.3',
+      version='0.0.4',
       description='GA Geodesy Package',
       long_description='A toolkit for Geodesy and Surveying in Python',
       url='https://github.com/GeoscienceAustralia/GeodePy',
