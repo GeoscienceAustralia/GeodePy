92af48d754b11c44efb913f2b890245892c24d75
physsi07 <starlinu33@gmail.com>
2019-10-07 00:10:27 -0400

    Issue #82: Preparing to test the functions, reading docs

---
+++ geodepy/tests/test_statistics.py
@@ -0,0 +1,22 @@
+def test_rotation_matrix():
+    pass
+
+
+def test_vcv_cart2local():
+    pass
+
+
+def test_vcv_local2cart2():
+    pass
+
+
+def test_error_ellipse():
+    pass
+
+
+def test_circ_hz_pu():
+    pass
+
+
+def test_k_val95():
+    pass
