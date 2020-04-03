657b04a22a936ba2d90637b9203d79a326ebbb49
harry093 <craigdharrison@icloud.com>
2020-04-03 18:35:10 +1100

    Changed to testing matrices with np.testing.assert_alomst_equal

--- geodepy/tests/test_statistics.py
+++ geodepy/tests/test_statistics.py
@@ -34,9 +34,8 @@ class TestStatistics(unittest.TestCase):
         ])
 
         result = statistics.vcv_cart2local(v_cart, lat, lon)
-        print(result)
 
-        self.assertAlmostEqual(expected_result, result, 6)
+        np.testing.assert_almost_equal(expected_result, result)
         self.assertEqual(type(expected_result), type(result))
 
     def test_vcv_cart2local_and_vcv_local2cart2_3X2(self):
