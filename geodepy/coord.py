3bb635d72a93e1f248c4d9787a223ad7745ca451
BatchelorJ <35472516+batchelorj@users.noreply.github.com>
2020-09-29 17:19:29 +1000

    Adding basic overloads to CoordGeo and CoordCart, conversion between these, test coverage
    Signed-off-by: SGV-Geodesy <56060078+SGV-Geodesy@users.noreply.github.com>

--- geodepy/coord.py
+++ geodepy/coord.py
@@ -6,7 +6,8 @@ Coordinate Module
 """
 
 from geodepy.constants import Projection, utm, grs80
-from geodepy.angles import angular_typecheck
+from geodepy.angles import DECAngle, angular_typecheck
+from geodepy.convert import xyz2llh, llh2xyz
 
 
 class CoordCart(object):
@@ -17,7 +18,7 @@ class CoordCart(object):
     at the coordinate that a reference surface (typically a geoid) is above
     or below an ellipsoid
     """
-    def __init__(self, xaxis, yaxis, zaxis, nval=0):
+    def __init__(self, xaxis, yaxis, zaxis, nval=None):
         """
         :param xaxis: Distance (m) along X Axis (positive when passing through
         intersection of equator and prime meridian)
@@ -35,11 +36,45 @@ class CoordCart(object):
         self.xaxis = float(xaxis)
         self.yaxis = float(yaxis)
         self.zaxis = float(zaxis)
-        self.nval = float(nval)
+        if not nval:
+            self.nval = None
+        else:
+            self.nval = float(nval)
 
     def __repr__(self):
         return (f'CoordCart: X: {self.xaxis} Y: {self.yaxis} Z: {self.zaxis} '
                 f'NVal: {self.nval}')
+
+    def __eq__(self, other):
+        if isinstance(other, CoordCart):
+            return vars(self) == vars(other)
+        else:
+            raise ValueError(f"Can't compare {self} to {other}. If other object"
+                             f" is coord, convert to CoordCart first.")
+
+    def __round__(self, n=None):
+        if self.nval is None:
+            return CoordCart(round(self.xaxis, n), round(self.yaxis, n),
+                             round(self.zaxis, n))
+        else:
+            return CoordCart(round(self.xaxis, n), round(self.yaxis, n),
+                             round(self.zaxis, n), round(self.nval, n))
+
+    def geo(self, ellipsoid=grs80):
+        """
+        Convert coordinates to Geographic
+        Note: If no N Value, no Orthometric Height set.
+        :param ellipsoid: geodepy.constants.Ellipsoid Object (default: grs80)
+        :return: Geographic Coordinate
+        :rtype: CoordGeo
+        """
+        lat, lon, ell_ht = xyz2llh(self.xaxis, self.yaxis,
+                                   self.zaxis, ellipsoid)
+        if self.nval is None:
+            return CoordGeo(DECAngle(lat), DECAngle(lon), ell_ht)
+        else:
+            return CoordGeo(DECAngle(lat), DECAngle(lon), ell_ht,
+                            ell_ht + self.nval)
 
 
 class CoordGeo(object):
@@ -50,7 +85,7 @@ class CoordGeo(object):
     Orthometric heights (m) are relative to a difference reference surface
     (typically a geoid).
     """
-    def __init__(self, lat, lon, ell_ht=0, orth_ht=0):
+    def __init__(self, lat, lon, ell_ht=None, orth_ht=None):
         """
         :param lat: Geographic Latitude (angle between +180 and -180 degrees,
         positive values are east of central meridian)
@@ -65,12 +100,56 @@ class CoordGeo(object):
         """
         self.lat = angular_typecheck(lat)
         self.lon = angular_typecheck(lon)
-        self.ell_ht = float(ell_ht)
-        self.orth_ht = float(orth_ht)
+        if ell_ht is None:
+            self.ell_ht = None
+        else:
+            self.ell_ht = float(ell_ht)
+        if orth_ht is None:
+            self.orth_ht = None
+        else:
+            self.orth_ht = float(orth_ht)
 
     def __repr__(self):
         return (f'CoordGeo: Lat: {self.lat} Lon: {self.lon} '
                 f'Ell_Ht: {self.ell_ht} Orth_Ht: {self.orth_ht}')
+
+    def __eq__(self, other):
+        if isinstance(other, CoordGeo):
+            return vars(self) == vars(other)
+        else:
+            raise ValueError(f"Can't compare {self} to {other}. If other object"
+                             f" is coord, convert to CoordGeo first.")
+
+    def __round__(self, n=None):
+        if self.ell_ht is None and self.orth_ht is None:
+            return CoordGeo(round(self.lat, n), round(self.lon, n))
+        elif self.ell_ht is None:
+            return CoordGeo(round(self.lat, n), round(self.lon, n),
+                            None, round(self.orth_ht, n))
+        elif self.orth_ht is None:
+            return CoordGeo(round(self.lat, n), round(self.lon, n),
+                            round(self.ell_ht, n), None)
+        else:
+            return CoordGeo(round(self.lat, n), round(self.lon, n),
+                            round(self.ell_ht, n), round(self.orth_ht, n))
+
+    def cart(self, ellipsoid=grs80):
+        """
+        Convert coordinates to Cartesian
+        Note: If no ellipsoid height set, uses 0m. No N Value output
+        :param ellipsoid: geodepy.constants.Ellipsoid Object (default: grs80)
+        :return: Cartesian Coordinate
+        :rtype: CoordCart
+        """
+        if self.ell_ht:
+            x, y, z = llh2xyz(self.lat, self.lon, self.ell_ht, ellipsoid)
+            if self.orth_ht:  # Only N Value if both Ellipsoid and Ortho Heights
+                return CoordCart(x, y, z, self.orth_ht - self.ell_ht)
+            else:  # No Ortho Height -> No N Value
+                return CoordCart(x, y, z)
+        else:  # No Ellipsoid Height - set to 0m for conversion
+            x, y, z = llh2xyz(self.lat, self.lon, 0, ellipsoid)
+            return CoordCart(x, y, z)  # No Ellipsoid Height -> No N Value
 
 
 class CoordTM(object):
@@ -83,7 +162,7 @@ class CoordTM(object):
     are relative to the surface of the ellipsoid. Orthometric heights (m) are
     relative to a difference reference surface (typically a geoid).
     """
-    def __init__(self, zone, east, north, ell_ht=0, orth_ht=0,
+    def __init__(self, zone, east, north, ell_ht=None, orth_ht=None,
                  hemi_north=False, projection=utm):
         """
         :param zone: Transverse Mercator Zone Number - 1 to 60
