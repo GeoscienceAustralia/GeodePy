957a0854c3a9d0f9d08fa00091bb4da55abbf5af
Craig Harrison <harry093@users.noreply.github.com>
2022-06-15 13:24:25 +1000

    Increment version to v0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='geodepy',
-      version='0.0.29',
+      version='0.1.0',
       description='GA Geodesy Package',
       long_description='A toolkit for Geodesy and Surveying in Python',
       url='https://github.com/GeoscienceAustralia/GeodePy',
