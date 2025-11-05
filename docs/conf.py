c3c0e9bdf76e114cd21dfe1df148eee03cfe46c4
KyranCookGA <kyran.cook@ga.gov.au>
2025-11-05 05:06:14 +0000

    Added autogenerated Sphinx documentation build files and new dark logo.

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GeodePy'
+copyright = '2025, Geoscience Australia'
+author = 'Geoscience Australia'
+release = '0.6.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_theme_options = {
+    "light_logo": "geodepy-logo.png",
+    "dark_logo": "geodepy-logo-dark.png",
+}
